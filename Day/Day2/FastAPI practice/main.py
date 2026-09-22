2db23ea446b790b68cd352954eeb37441df92afa
bindhushri2246 <bindhushri2246@gmail.com>
2026-09-22 13:29:05 +0530

    path parameters with type hint

--- Day/Day2/FastAPI practice/main.py
+++ Day/Day2/FastAPI practice/main.py
@@ -18,4 +18,10 @@ def create_something():
 @app.get("/student/{usn}")
 def get_result(usn):
     return{"Result:":"Distinction","usn":usn}
+
+#path parameters with type hint 
+@app.get("/candidate/{rollno}")
+def get_candidate(rollno:int):
+    return{"Result:":"Distinction","rollno":rollno,"type":str(type)(rollno)}
+ 
  
