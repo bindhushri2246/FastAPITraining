57668d66fd0e89d54375fd06f40cbc3a649300aa
bindhushri2246 <bindhushri2246@gmail.com>
2026-09-22 13:07:47 +0530

    post request

---
+++ Day/Day2/FastAPI practice/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+app = FastAPI()
+@app.get("/")
+def home():
+    return {"page":"home"}
+@app.get("/about")
+def about():
+    return{"page":"About","author":"Charitha"}
+@app.get("/health")
+def health():
+    return{"status":"ok"}
+#post request
+@app.post("/create")
+def create_something():
+    return {"message:created"} 
